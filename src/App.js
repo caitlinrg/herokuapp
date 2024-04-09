83710f90058a2d60b9ee8ab260305e8a8d7a4f5e
caitlinrg <caitlin@revenueguru.nl>
2024-04-09 17:02:27 +0200

    MEWS api connection FetchAPI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetch("https://api.mews-demo.com/api/connector/v1/configuration/get", {
+    method: "POST", 
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: {
+        "ClientToken": "E0D439EE522F44368DC78E1BFB03710C-D24FB11DBE31D4621C4817E028D9E1D",
+        "AccessToken": "7059D2C25BF64EA681ACAB3A00B859CC-D91BFF2B1E3047A3E0DEC1D57BE1382",
+        "Client": "NameOfYourCompanyOrApplication"
+    }
+    
+  })
+      .then(res => {
+        return res.json()
+      })
+      .then(data => console.log(data))
+
   return (
     <div className="App">
       <header className="App-header">
